1859f1fe38a74b64837bdee86cac795f3d25d9c1
oscar_agreda <none@none.com>
2009-10-03 03:33:31 +0000

    Garth and I, separated some modules and created a few navigation views

---
+++ XERPsvn/XERP.Module.Web/WebModule.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel;
+
+using DevExpress.ExpressApp;
+
+namespace XERP.Module.Web
+{
+    [ToolboxItemFilter("Xaf.Platform.Web")]
+    public sealed partial class XERPAspNetModule : ModuleBase
+    {
+        public XERPAspNetModule()
+        {
+            InitializeComponent();
+        }
+    }
+}
