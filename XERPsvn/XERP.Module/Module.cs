30778aad92fa8684cb83753df8f0439d7e342b02
oscar_agreda <none@none.com>
2009-10-03 03:33:31 +0000

    Garth and I, separated some modules and created a few navigation views

---
+++ XERPsvn/XERP.Module/Module.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+using System.Reflection;
+
+
+namespace XERP.Module
+{
+    public sealed partial class XERPModule : ModuleBase
+    {
+        public XERPModule()
+        {
+            InitializeComponent();
+        }
+    }
+}
