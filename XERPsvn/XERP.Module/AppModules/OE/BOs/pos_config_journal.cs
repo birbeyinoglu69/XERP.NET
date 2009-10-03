3005ed4eab5ec801f9cc071ea46e4f410912c66f
oscar_agreda <none@none.com>
2009-10-03 03:33:31 +0000

    Garth and I, separated some modules and created a few navigation views

---
+++ XERPsvn/XERP.Module/AppModules/OE/BOs/pos_config_journal.cs
@@ -0,0 +1,101 @@
+﻿
+using System;
+using System.ComponentModel;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Persistent.Base.General;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+
+namespace XERP
+{
+
+
+
+    [DefaultClassOptions]
+    [DeferredDeletion(false)]
+	[DefaultProperty("code")]
+    [Persistent("pos_config_journal")]
+	public partial class pos_config_journal : XPCustomObject
+	{
+		#region Properties
+	    private System.Int32 fid;
+        [Key(AutoGenerate = true), Browsable(false)]
+        //pos_config_journal_id
+		public System.Int32 id {
+			get { return fid; }
+			set { SetPropertyValue("id", ref fid, value); }
+		}
+        
+            private res_users fcreate_uid;
+            //FK FK_pos_config_journal_create_uid
+            [Custom("Caption", "Create Uid")]
+            public res_users create_uid {
+                get { return fcreate_uid; }
+                set { SetPropertyValue<res_users>("create_uid", ref fcreate_uid, value); }
+            }
+    
+            private DateTime? fcreate_date;
+            [Custom("Caption", "Create Date")]
+            public DateTime? create_date {
+                get { return fcreate_date; }
+                set { SetPropertyValue("create_date", ref fcreate_date, value); }
+            }
+    
+            private DateTime? fwrite_date;
+            [Custom("Caption", "Write Date")]
+            public DateTime? write_date {
+                get { return fwrite_date; }
+                set { SetPropertyValue("write_date", ref fwrite_date, value); }
+            }
+    
+        
+            private res_users fwrite_uid;
+            //FK FK_pos_config_journal_write_uid
+            [Custom("Caption", "Write Uid")]
+            public res_users write_uid {
+                get { return fwrite_uid; }
+                set { SetPropertyValue<res_users>("write_uid", ref fwrite_uid, value); }
+            }
+    
+            private System.String fcode;
+            [Size(64)]
+            [Custom("Caption", "Code")]
+            public System.String code {
+                get { return fcode; }
+                set { SetPropertyValue("code", ref fcode, value); }
+            }
+    
+            private System.String fname;
+            [Size(64)]
+            [Custom("Caption", "Name")]
+            public System.String name {
+                get { return fname; }
+                set { SetPropertyValue("name", ref fname, value); }
+            }
+    
+        
+            private account_journal fjournal_id;
+            //FK FK_pos_config_journal_journal_id
+            [Custom("Caption", "Journal Id")]
+            public account_journal journal_id {
+                get { return fjournal_id; }
+                set { SetPropertyValue<account_journal>("journal_id", ref fjournal_id, value); }
+            }
+    
+		#endregion
+	
+		#region Collections
+		#endregion
+	
+		#region Constructors
+		public pos_config_journal(Session session) : base(session) { }
+        #endregion
+
+	}
+}
+//Generated for XERP
+
