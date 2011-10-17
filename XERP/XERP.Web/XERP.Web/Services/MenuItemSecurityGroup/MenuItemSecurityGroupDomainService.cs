11110e201fe69628f288b30ea1232afddb9be722
MattPaulson <none@none.com>
2011-10-17 20:27:10 +0000

    Got this working rough frame of menu maintenance...

---
+++ XERP/XERP.Web/XERP.Web/Services/MenuItemSecurityGroup/MenuItemSecurityGroupDomainService.cs
@@ -0,0 +1,327 @@
+﻿
+namespace XERP.Web.Services.MenuItemSecurityGroup
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Data;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.EntityFramework;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+    using XERP.Web.Models.MenuItemSecurityGroup;
+
+
+    // Implements application logic using the MenuItemSecurityGroupEntities context.
+    // TODO: Add your application logic to these methods or in additional methods.
+    // TODO: Wire up authentication (Windows/ASP.NET Forms) and uncomment the following to disable anonymous access
+    // Also consider adding roles to restrict access as appropriate.
+    // [RequiresAuthentication]
+    [EnableClientAccess()]
+    public partial class MenuItemSecurityGroupDomainService : LinqToEntitiesDomainService<MenuItemSecurityGroupEntities>
+    {
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'Companies' query.
+        public IQueryable<Company> GetCompanies()
+        {
+            return this.ObjectContext.Companies;
+        }
+        //Modified with Order By and Moved to Extended.cs
+        //// TODO:  
+        //// Consider constraining the results of your query method.  If you need additional input you can
+        //// add parameters to this method or create additional query methods with different names.
+        //// To support paging you will need to add ordering to the 'MenuItems' query.
+        public IQueryable<MenuItem> GetMenuItems()
+        {
+            return this.ObjectContext.MenuItems;
+        }
+
+        public void InsertMenuItem(MenuItem menuItem)
+        {
+            if ((menuItem.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(menuItem, EntityState.Added);
+            }
+            else
+            {
+                this.ObjectContext.MenuItems.AddObject(menuItem);
+            }
+        }
+
+        public void UpdateMenuItem(MenuItem currentMenuItem)
+        {
+            this.ObjectContext.MenuItems.AttachAsModified(currentMenuItem, this.ChangeSet.GetOriginal(currentMenuItem));
+        }
+
+        public void DeleteMenuItem(MenuItem menuItem)
+        {
+            if ((menuItem.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(menuItem, EntityState.Deleted);
+            }
+            else
+            {
+                this.ObjectContext.MenuItems.Attach(menuItem);
+                this.ObjectContext.MenuItems.DeleteObject(menuItem);
+            }
+        }
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'MenuItemCodes' query.
+        public IQueryable<MenuItemCode> GetMenuItemCodes()
+        {
+            return this.ObjectContext.MenuItemCodes;
+        }
+
+        public void InsertMenuItemCode(MenuItemCode menuItemCode)
+        {
+            if ((menuItemCode.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(menuItemCode, EntityState.Added);
+            }
+            else
+            {
+                this.ObjectContext.MenuItemCodes.AddObject(menuItemCode);
+            }
+        }
+
+        public void UpdateMenuItemCode(MenuItemCode currentMenuItemCode)
+        {
+            this.ObjectContext.MenuItemCodes.AttachAsModified(currentMenuItemCode, this.ChangeSet.GetOriginal(currentMenuItemCode));
+        }
+
+        public void DeleteMenuItemCode(MenuItemCode menuItemCode)
+        {
+            if ((menuItemCode.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(menuItemCode, EntityState.Deleted);
+            }
+            else
+            {
+                this.ObjectContext.MenuItemCodes.Attach(menuItemCode);
+                this.ObjectContext.MenuItemCodes.DeleteObject(menuItemCode);
+            }
+        }
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'MenuItemTypes' query.
+        public IQueryable<MenuItemType> GetMenuItemTypes()
+        {
+            return this.ObjectContext.MenuItemTypes;
+        }
+
+        public void InsertMenuItemType(MenuItemType menuItemType)
+        {
+            if ((menuItemType.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(menuItemType, EntityState.Added);
+            }
+            else
+            {
+                this.ObjectContext.MenuItemTypes.AddObject(menuItemType);
+            }
+        }
+
+        public void UpdateMenuItemType(MenuItemType currentMenuItemType)
+        {
+            this.ObjectContext.MenuItemTypes.AttachAsModified(currentMenuItemType, this.ChangeSet.GetOriginal(currentMenuItemType));
+        }
+
+        public void DeleteMenuItemType(MenuItemType menuItemType)
+        {
+            if ((menuItemType.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(menuItemType, EntityState.Deleted);
+            }
+            else
+            {
+                this.ObjectContext.MenuItemTypes.Attach(menuItemType);
+                this.ObjectContext.MenuItemTypes.DeleteObject(menuItemType);
+            }
+        }
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'MenuSecurities' query.
+        public IQueryable<MenuSecurity> GetMenuSecurities()
+        {
+            return this.ObjectContext.MenuSecurities;
+        }
+
+        public void InsertMenuSecurity(MenuSecurity menuSecurity)
+        {
+            if ((menuSecurity.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(menuSecurity, EntityState.Added);
+            }
+            else
+            {
+                this.ObjectContext.MenuSecurities.AddObject(menuSecurity);
+            }
+        }
+
+        public void UpdateMenuSecurity(MenuSecurity currentMenuSecurity)
+        {
+            this.ObjectContext.MenuSecurities.AttachAsModified(currentMenuSecurity, this.ChangeSet.GetOriginal(currentMenuSecurity));
+        }
+
+        public void DeleteMenuSecurity(MenuSecurity menuSecurity)
+        {
+            if ((menuSecurity.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(menuSecurity, EntityState.Deleted);
+            }
+            else
+            {
+                this.ObjectContext.MenuSecurities.Attach(menuSecurity);
+                this.ObjectContext.MenuSecurities.DeleteObject(menuSecurity);
+            }
+        }
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'SecurityGroups' query.
+        public IQueryable<SecurityGroup> GetSecurityGroups()
+        {
+            return this.ObjectContext.SecurityGroups;
+        }
+
+        public void InsertSecurityGroup(SecurityGroup securityGroup)
+        {
+            if ((securityGroup.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(securityGroup, EntityState.Added);
+            }
+            else
+            {
+                this.ObjectContext.SecurityGroups.AddObject(securityGroup);
+            }
+        }
+
+        public void UpdateSecurityGroup(SecurityGroup currentSecurityGroup)
+        {
+            this.ObjectContext.SecurityGroups.AttachAsModified(currentSecurityGroup, this.ChangeSet.GetOriginal(currentSecurityGroup));
+        }
+
+        public void DeleteSecurityGroup(SecurityGroup securityGroup)
+        {
+            if ((securityGroup.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(securityGroup, EntityState.Deleted);
+            }
+            else
+            {
+                this.ObjectContext.SecurityGroups.Attach(securityGroup);
+                this.ObjectContext.SecurityGroups.DeleteObject(securityGroup);
+            }
+        }
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'SecurityGroupCodes' query.
+        public IQueryable<SecurityGroupCode> GetSecurityGroupCodes()
+        {
+            return this.ObjectContext.SecurityGroupCodes;
+        }
+
+        public void InsertSecurityGroupCode(SecurityGroupCode securityGroupCode)
+        {
+            if ((securityGroupCode.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(securityGroupCode, EntityState.Added);
+            }
+            else
+            {
+                this.ObjectContext.SecurityGroupCodes.AddObject(securityGroupCode);
+            }
+        }
+
+        public void UpdateSecurityGroupCode(SecurityGroupCode currentSecurityGroupCode)
+        {
+            this.ObjectContext.SecurityGroupCodes.AttachAsModified(currentSecurityGroupCode, this.ChangeSet.GetOriginal(currentSecurityGroupCode));
+        }
+
+        public void DeleteSecurityGroupCode(SecurityGroupCode securityGroupCode)
+        {
+            if ((securityGroupCode.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(securityGroupCode, EntityState.Deleted);
+            }
+            else
+            {
+                this.ObjectContext.SecurityGroupCodes.Attach(securityGroupCode);
+                this.ObjectContext.SecurityGroupCodes.DeleteObject(securityGroupCode);
+            }
+        }
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'SecurityGroupTypes' query.
+        public IQueryable<SecurityGroupType> GetSecurityGroupTypes()
+        {
+            return this.ObjectContext.SecurityGroupTypes;
+        }
+
+        public void InsertSecurityGroupType(SecurityGroupType securityGroupType)
+        {
+            if ((securityGroupType.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(securityGroupType, EntityState.Added);
+            }
+            else
+            {
+                this.ObjectContext.SecurityGroupTypes.AddObject(securityGroupType);
+            }
+        }
+
+        public void UpdateSecurityGroupType(SecurityGroupType currentSecurityGroupType)
+        {
+            this.ObjectContext.SecurityGroupTypes.AttachAsModified(currentSecurityGroupType, this.ChangeSet.GetOriginal(currentSecurityGroupType));
+        }
+
+        public void DeleteSecurityGroupType(SecurityGroupType securityGroupType)
+        {
+            if ((securityGroupType.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(securityGroupType, EntityState.Deleted);
+            }
+            else
+            {
+                this.ObjectContext.SecurityGroupTypes.Attach(securityGroupType);
+                this.ObjectContext.SecurityGroupTypes.DeleteObject(securityGroupType);
+            }
+        }
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'SystemUsers' query.
+        public IQueryable<SystemUser> GetSystemUsers()
+        {
+            return this.ObjectContext.SystemUsers;
+        }
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'SystemUserSecurities' query.
+        public IQueryable<SystemUserSecurity> GetSystemUserSecurities()
+        {
+            return this.ObjectContext.SystemUserSecurities;
+        }
+    }
+}
+
+
---
+++ XERP/XERP.Web/XERP.Web/Services/Plant/PlantDomainService.cs
@@ -0,0 +1,177 @@
+﻿//If Schema changes are required all the canned code below will be lost...
+//Do not modify this Class use the partial extended class
+//If one of the canned CRUD Methods below needs modification
+//Cut and paste it to the extended Partial Class and modify their
+namespace XERP.Web.Services.Plant
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Data;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.EntityFramework;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+    using XERP.Web.Models.Plant;
+    using System.Configuration;
+    using System.Data.EntityClient;
+
+
+    // Implements application logic using the PlantEntities context.
+    // TODO: Add your application logic to these methods or in additional methods.
+    // TODO: Wire up authentication (Windows/ASP.NET Forms) and uncomment the following to disable anonymous access
+    // Also consider adding roles to restrict access as appropriate.
+    // [RequiresAuthentication]
+    [EnableClientAccess()]
+    public partial class PlantDomainService : LinqToEntitiesDomainService<PlantEntities>
+    {
+        
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'Addresses' query.
+        public IQueryable<Address> GetAddresses()
+        {
+            return this.ObjectContext.Addresses;
+        }
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'Companies' query.
+        public IQueryable<Company> GetCompanies()
+        {
+            return this.ObjectContext.Companies;
+        }
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'Plants' query.
+        public IQueryable<Plant> GetPlants()
+        {
+            return this.ObjectContext.Plants;
+        }
+
+        public void InsertPlant(Plant plant)
+        {
+            if ((plant.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(plant, EntityState.Added);
+            }
+            else
+            {
+                this.ObjectContext.Plants.AddObject(plant);
+            }
+        }
+
+        public void UpdatePlant(Plant currentPlant)
+        {
+            this.ObjectContext.Plants.AttachAsModified(currentPlant, this.ChangeSet.GetOriginal(currentPlant));
+        }
+
+        public void DeletePlant(Plant plant)
+        {
+            if ((plant.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(plant, EntityState.Deleted);
+            }
+            else
+            {
+                this.ObjectContext.Plants.Attach(plant);
+                this.ObjectContext.Plants.DeleteObject(plant);
+            }
+        }
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'PlantCodes' query.
+        public IQueryable<PlantCode> GetPlantCodes()
+        {
+            return this.ObjectContext.PlantCodes;
+        }
+
+        public void InsertPlantCode(PlantCode plantCode)
+        {
+            if ((plantCode.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(plantCode, EntityState.Added);
+            }
+            else
+            {
+                this.ObjectContext.PlantCodes.AddObject(plantCode);
+            }
+        }
+
+        public void UpdatePlantCode(PlantCode currentPlantCode)
+        {
+            this.ObjectContext.PlantCodes.AttachAsModified(currentPlantCode, this.ChangeSet.GetOriginal(currentPlantCode));
+        }
+
+        public void DeletePlantCode(PlantCode plantCode)
+        {
+            if ((plantCode.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(plantCode, EntityState.Deleted);
+            }
+            else
+            {
+                this.ObjectContext.PlantCodes.Attach(plantCode);
+                this.ObjectContext.PlantCodes.DeleteObject(plantCode);
+            }
+        }
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'PlantTypes' query.
+        public IQueryable<PlantType> GetPlantTypes()
+        {
+            return this.ObjectContext.PlantTypes;
+        }
+
+        public void InsertPlantType(PlantType plantType)
+        {
+            if ((plantType.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(plantType, EntityState.Added);
+            }
+            else
+            {
+                this.ObjectContext.PlantTypes.AddObject(plantType);
+            }
+        }
+
+        public void UpdatePlantType(PlantType currentPlantType)
+        {
+            this.ObjectContext.PlantTypes.AttachAsModified(currentPlantType, this.ChangeSet.GetOriginal(currentPlantType));
+        }
+
+        public void DeletePlantType(PlantType plantType)
+        {
+            if ((plantType.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(plantType, EntityState.Deleted);
+            }
+            else
+            {
+                this.ObjectContext.PlantTypes.Attach(plantType);
+                this.ObjectContext.PlantTypes.DeleteObject(plantType);
+            }
+        }
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'Warehouses' query.
+        public IQueryable<Warehouse> GetWarehouses()
+        {
+            return this.ObjectContext.Warehouses;
+        }
+    }
+
+}
+
+
