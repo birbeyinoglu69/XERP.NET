79a4de43132f018fedb6e5f9f2887bf94a8d293d
MattPaulson <none@none.com>
2011-10-17 20:27:10 +0000

    Got this working rough frame of menu maintenance...

---
+++ XERP/XERP.Web/XERP/ClientModels/NestedMenuItem/NestedMenuItem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using XERP.Web.Models.MenuItemSecurityGroup;
+namespace XERP.ClientModels.NestedMenuItem
+{
+    public class NestedMenuItem
+    {
+        public string CompanyID { get; set; }
+        public string MenuItemID { get; set; }
+        public string ParentMenuID { get; set; }
+        public Int64 AutoID { get; set; }
+        public ObservableCollection<NestedMenuItem> Children { get; set; }
+
+        public NestedMenuItem(string cid, string mid, string pid, Int64 aid)
+        {
+            CompanyID = cid;
+            MenuItemID = mid;
+            ParentMenuID = pid;
+            AutoID = aid;
+            Children = new ObservableCollection<NestedMenuItem>();
+        }
+        public NestedMenuItem(MenuItem mi)
+        {
+            CompanyID = mi.CompanyID;
+            MenuItemID = mi.MenuItemID;
+            ParentMenuID = mi.ParentMenuID;
+            AutoID = mi.AutoID;
+            Children = new ObservableCollection<NestedMenuItem>();
+        }
+        public NestedMenuItem(NestedMenuItem nmi)
+        {
+            CompanyID = nmi.CompanyID;
+            MenuItemID = nmi.MenuItemID;
+            ParentMenuID = nmi.ParentMenuID;
+            AutoID = nmi.AutoID;
+            Children = new ObservableCollection<NestedMenuItem>();
+        }
+        public NestedMenuItem(NestedMenuItem nmi, params NestedMenuItem[] cnmis)
+        {
+            CompanyID = nmi.CompanyID;
+            MenuItemID = nmi.MenuItemID;
+            ParentMenuID = nmi.ParentMenuID;
+            AutoID = nmi.AutoID;
+            ObservableCollection<NestedMenuItem> oc = new ObservableCollection<NestedMenuItem>();
+            foreach (var cnmi in cnmis)
+                oc.Add(cnmi);
+            Children = oc;
+        }
+
+    }      
+}
