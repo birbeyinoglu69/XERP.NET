c991f33179f1e49bb7ada070a51bc0d51e7aecf8
MattPaulson <none@none.com>
2011-10-17 18:38:37 +0000

    Yes still checking this in need some help...

---
+++ XERP/Bin/XERP.Web/Services/Plant/PlantDomainServiceExtended.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.ServiceModel.DomainServices.EntityFramework;
+using System.ServiceModel.DomainServices.Hosting;
+using System.ServiceModel.DomainServices.Server;
+using XERP.Web.Models.Plant;
+using System.Configuration;
+using System.Data.EntityClient;
+//All custom code should be placed in this partial class
+namespace XERP.Web.Services.Plant
+{
+    public partial class PlantDomainService
+    {//Do not modify this method it is required...
+        //Use shared global SQL Connection string
+        protected override PlantEntities CreateObjectContext()
+        {
+            string baseSQLConnectionString = ConfigurationManager.ConnectionStrings["BaseSQL"].ConnectionString;
+            EntityConnectionStringBuilder entityConectionString = new EntityConnectionStringBuilder(ConfigurationManager.ConnectionStrings["PlantEntities"].ToString());
+            entityConectionString.ProviderConnectionString = baseSQLConnectionString;
+            //return base.CreateObjectContext();
+            return new PlantEntities(entityConectionString.ConnectionString);
+        }
+    }
+}
---
+++ XERP/Bin/XERP.Web/Services/SystemUser/SystemUserDomainServiceExtended.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.ServiceModel.DomainServices.EntityFramework;
+using System.ServiceModel.DomainServices.Hosting;
+using System.ServiceModel.DomainServices.Server;
+using XERP.Web.Models.SystemUser;
+using System.Configuration;
+using System.Data.EntityClient;
+//All custom code should be placed in this partial class
+namespace XERP.Web.Services.SystemUser
+{
+    public partial class SystemUserDomainService
+    {//Do not modify this method it is required...
+        //Use shared global SQL Connection string
+        protected override SystemUserEntities CreateObjectContext()
+        {
+            string baseSQLConnectionString = ConfigurationManager.ConnectionStrings["BaseSQL"].ConnectionString;
+            EntityConnectionStringBuilder entityConectionString = new EntityConnectionStringBuilder(ConfigurationManager.ConnectionStrings["SystemUserEntities"].ToString());
+            entityConectionString.ProviderConnectionString = baseSQLConnectionString;
+            //return base.CreateObjectContext();
+            return new SystemUserEntities(entityConectionString.ConnectionString);
+        }
+    }
+}
