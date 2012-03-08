722b389f5d55e6ef017ae40e6d09e35dcaf2dfb5
MattPaulson <none@none.com>
2012-03-08 22:44:21 +0000

    Initial Build for Core XERP Items

---
+++ XERP/XERP/XERP.Client/XERP.Client.WPF/XERP.Client.WPF.LogIn/Locators/ViewModelLocator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+// Toolkit namespace
+using SimpleMvvmToolkit;
+
+//XERP namespaces
+using XERP.Client.WPF.LogIn.ViewModels;
+using XERP.LogInDomain.Services;
+
+namespace XERP.Client.WPF.LogIn
+{
+    public class ViewModelLocator
+    {
+        // Create MainPageViewModel on demand
+        public MainPageViewModel MainPageViewModel
+        {
+            get { return new MainPageViewModel(); }
+        }
+
+        public LogInViewModel LogInViewModel
+        {
+            get
+            {
+                ILogInServiceAgent serviceAgent = new LogInServiceAgent();
+                return new LogInViewModel(serviceAgent);
+            }
+        }
+
+    }
+}
