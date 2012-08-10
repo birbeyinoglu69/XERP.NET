3f8a6e98973e478a526a11a519243259e2b37c5f
MattPaulson <none@none.com>
2012-08-10 20:44:45 +0000

    Solid build of login, Main Menu with one module the Company Module.  Company Module will serve as the template for all future modules.  Once it is complete we can start on the 100's of modules required to make ERP function...

---
+++ XERP.Client/XERP.Client.WPF/XERP.Client.WPF.CompanyMaintenance/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+
+namespace XERP.Client.WPF.CompanyMaintenance
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+
+    }
+}
