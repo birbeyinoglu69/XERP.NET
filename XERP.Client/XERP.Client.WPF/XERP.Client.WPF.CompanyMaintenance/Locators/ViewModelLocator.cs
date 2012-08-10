5fba7e41fd72327a426ff0b9819c371556265de0
MattPaulson <none@none.com>
2012-08-10 20:44:45 +0000

    Solid build of login, Main Menu with one module the Company Module.  Company Module will serve as the template for all future modules.  Once it is complete we can start on the 100's of modules required to make ERP function...

---
+++ XERP.Client/XERP.Client.WPF/XERP.Client.WPF.CompanyMaintenance/Locators/ViewModelLocator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+// Toolkit namespace
+using SimpleMvvmToolkit;
+
+//XERP namespaces
+using XERP.Client.WPF.CompanyMaintenance.ViewModels;
+using XERP.Domain.CompanyDomain.Services;
+
+namespace XERP.Client.WPF.CompanyMaintenance
+{
+    public class ViewModelLocator
+    {
+        //public MainPageViewModel MainPageViewModel
+        //{
+        //    get { return new MainPageViewModel(); }
+        //}
+
+        public MainMaintenanceViewModel MainMaintenanceViewModel
+        {
+            get
+            {
+                ICompanyServiceAgent serviceAgent = new CompanyServiceAgent();
+                return new MainMaintenanceViewModel(serviceAgent);
+            }
+        }
+
+        public MainSearchViewModel MainSearchViewModel
+        {
+            get
+            {
+                ICompanyServiceAgent serviceAgent = new CompanyServiceAgent();
+                return new MainSearchViewModel(serviceAgent);
+            }
+        }
+
+        public TypeSearchViewModel TypeSearchViewModel
+        {
+            get
+            {
+                ICompanyServiceAgent serviceAgent = new CompanyServiceAgent();
+                return new TypeSearchViewModel(serviceAgent);
+            }
+        }
+
+        public TypeMaintenanceViewModel TypeMaintenanceViewModel
+        {
+            get
+            {
+                ICompanyServiceAgent serviceAgent = new CompanyServiceAgent();
+                return new TypeMaintenanceViewModel(serviceAgent);
+            }
+        }
+    }
+}
