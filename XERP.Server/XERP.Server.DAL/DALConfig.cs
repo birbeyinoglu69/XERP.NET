8cea43bfb0e202836fe447e4cde9e74289728fee
MattPaulson <none@none.com>
2012-08-13 22:16:38 +0000

    most recent build

---
+++ XERP.Server/XERP.Server.DAL/DALConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.EntityClient;
+using System.Data.SqlClient;
+
+namespace XERP.Server.DAL
+{
+    public class DALConfig
+    {
+        private const string _providerName = "System.Data.SQLClient";
+        public string ProviderName
+        {
+            get { return _providerName; }
+        }
+
+        public string BaseSQLConnectionString
+        {
+            get
+            {
+                XERPServerConfig config = new XERPServerConfig();
+                return config.BaseSQLConnectionString;
+            }
+            set
+            {
+                XERPServerConfig config = new XERPServerConfig();
+                config.BaseSQLConnectionString = value;
+            }
+        }
+        public DALConfig()
+        {
+
+        }
+    }
+}
